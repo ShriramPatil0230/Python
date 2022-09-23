dd7465e3048bb4638ba6d476cfd7a1af8e0bac0d
Shriram Patil <shrirampatil246@gmail.com>
2022-09-23 18:37:12 +0530

    Add files via upload

---
+++ 02_addtwonumber.py
@@ -0,0 +1,9 @@
+# add  number
+a=int(input("Enter your first number:"))
+b=int(input("Enter your second number:"))
+c=int(input("Enter your third number:"))
+
+Sum = a+b+c
+print("Sum =",Sum)
+
+   
