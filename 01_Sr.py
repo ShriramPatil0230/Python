4900f17d8ba9b29cb801a88db845db7f3a7dd610
Shriram Patil <shrirampatil246@gmail.com>
2022-09-23 18:39:39 +0530

    Update 01_Sr.py

--- 01_Sr.py
+++ 01_Sr.py
@@ -9,11 +9,15 @@ age-20 DOB-02/04/2001''' ) #''' are use to write multiple lines in string
 a = "Shriram"   #string
 b = 246         #integer
 c = 30.02       #float
+d = True
 print(a)
+print(b)
 print(c)
+print(d)
 
 #Printing type of variable
 
 print(type(a))
 print(type(b))
 print(type(c))
+print(type(d))
