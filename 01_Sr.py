5861ec0d773cd9faea083b3a8f3b88edc78cdbb9
Shriram Patil <shrirampatil246@gmail.com>
2022-09-23 18:36:53 +0530

    Add files via upload

---
+++ 01_Sr.py
@@ -0,0 +1,19 @@
+# lets learn python
+
+print("Hello Word") #("") are use to write string
+print('''Shriram Rajendra Patil 
+age-20 DOB-02/04/2001''' ) #''' are use to write multiple lines in string 
+
+# variables
+
+a = "Shriram"   #string
+b = 246         #integer
+c = 30.02       #float
+print(a)
+print(c)
+
+#Printing type of variable
+
+print(type(a))
+print(type(b))
+print(type(c))
