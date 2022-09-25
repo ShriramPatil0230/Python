a150492ecf757da81f91b5859f95e9b8eacf67ba
Shriram Patil <shrirampatil246@gmail.com>
2022-09-25 14:15:25 +0530

    Add files via upload

---
+++ 05_opreators.py
@@ -0,0 +1,21 @@
+#assignment opreators
+
+b = 246
+# b += 12
+print(b)
+
+# comparison operatoors
+
+print(b>143)
+
+print(b<143)
+
+# logical opreators
+
+bool1 = True
+bool2 = False
+
+print("The value of bool1 and bool2 is", (bool1 and bool2))
+print("The value of bool1 or bool2 is", (bool1 or bool2))
+print("The value of bool1 not bool2 is",( not bool2))        
+
