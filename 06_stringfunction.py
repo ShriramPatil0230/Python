287ecb5ae1a6a3722cdb6bc34254f16aa2a759b9
Shriram Patil <shrirampatil246@gmail.com>
2022-09-25 14:16:27 +0530

    Add files via upload

---
+++ 06_stringfunction.py
@@ -0,0 +1,27 @@
+ # string function 
+   
+story = "my name is shriram, computer enginer"
+print(story)
+
+print(len(story))
+      #len string is use to count number in string
+
+print(story.endswith("engineer"))
+     #endswith stirng is use to check ending word of string 
+
+print(story.count("i"))
+     #count string is use to count word 
+
+print(story.capitalize()) 
+     #capitalize string is use to capitalize starting word of string  
+
+print(story.find("Shriram"))
+print(story.find("shriram"))
+    #find string is use to find the word in string
+
+print(story.replace("shriram", "ram"))
+    # replace string is use to replace word in string
+
+    # solve problem
+# name = input("Enter your name:")
+# print("Good afternoon," + name)
