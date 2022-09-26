57a8689ebf959413891b499818f6fbbead7575fc
Shriram Patil <shrirampatil246@gmail.com>
2022-09-26 19:27:00 +0530

    Update 08_pythonlist.py

--- 08_pythonlist.py
+++ 08_pythonlist.py
@@ -1,15 +1,14 @@
-# Creat list using []
+  # Creat list using []
 a = [ 2, 30, "R", "S", "P"]
 print(a)
 
 print(a[0])
-#access using index using a[1],a[2] 
+  #access using index using a[1],a[2] 
 
 b = [230,"Shriram",True,2.30 ]
- # We can create list with different type
+  # We can create list with different type
 print(b)
 
-# list slicing
-
+  # list slicing
 print(b[:3])
 print(b[0:4])
