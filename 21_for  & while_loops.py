0f7afacb52f0b90bad8808a0708b6f603fb3cbb3
Shriram Patil <shrirampatil246@gmail.com>
2022-10-01 13:21:46 +0530

    Add files via upload

---
+++ 21_for  & while_loops.py
@@ -0,0 +1,19 @@
+# for loop in python 
+
+a1 = []
+for a in range(2,10,2):
+    print(a)
+    a1.append(a)
+print(a1)
+
+# while loop in python 
+ 
+# b = 1
+# while (b<=50):
+#     print(b)
+#     b = b + 1
+
+add2 = 2
+while (add2<=50):
+    print(add2)
+    add2 = add2 + 2
